90d15045d39c0a4fe59c6bbfc6e2a6372ab01f25
jaime1127 <84351799+jaime1127@users.noreply.github.com>
2025-10-21 01:53:18 -0400

    prettier on slug page

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -3,7 +3,11 @@ import { fetchNewContentPage } from "@/app/lib/content";
 import { ContentPage } from "@/app/ui/components/content";
 import { notFound } from "next/navigation";
 
-export default async function Page({ params }: { params: Promise<{ slug: string }> }) {
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
   const { slug } = await params;
 
   // Fetch data from Hygraph
